12d3890f16c96c8db4a6e595a1ab80f4db33605e
eqq-evil <99731961+eqq-evil@users.noreply.github.com>
2022-07-22 14:12:48 +0200

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,6 @@
+
+if game.PlaceId == 155615604 then
+    loadstring(game:HttpGetAsync(('https://raw.githubusercontent.com/eqq-evil/xmn/main/games/prisonlife.lua')))
+else
+    game.Players.LocalPlayer:Kick("Nieprawidlowa Gra! Wybierz Prison Life.")
+end
